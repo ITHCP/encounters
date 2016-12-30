1b5ad471a46e611533d287f0cb12fe245d0dfca8
Chris Compton <code@dccompton.com>
2016-12-30 08:26:13 -0600

    Pull from API

--- Playground.elm
+++ Playground.elm
@@ -1,31 +1,98 @@
 module Playground exposing (..) 
 
 import Config exposing (..)
-import Server exposing (..)
 
 import Http
-import Html exposing (..) 
-import Json.Decode exposing (..) 
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Json.Decode exposing (..)
 
+
+-- ###################################################
+-- ####  MODEL  ######################################
+-- ###################################################
+
+type alias Model = 
+    { 
+        json : String
+    }
+
+initialModel : Model
+initialModel = 
+    { 
+        json = ""
+    }
+
+-- ###################################################
+-- ####  UPDATE  #####################################   
+-- ###################################################
+
+type Msg 
+    = LoadJson (Result Http.Error String)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of    
+        LoadJson (result) ->
+            case result of 
+                    Ok jsonData ->
+                        let
+                            _ = Debug.log json
+                        in
+                            ({ model | json = jsonData }, Cmd.none)
+                    Err error ->
+                        let
+                            _ = Debug.log "ERROR: " error
+                        in
+                            (model, Cmd.none)  
+
+getJson : Cmd Msg
+getJson =
+    patientUrl
+        |> Http.getString
+        |> Http.send LoadJson
+
+
+json : String
 json = 
     """ 
     { "phrase": "Hello, World!" } 
     """ 
 
+decoder : Decoder String
 decoder = 
     field "phrase" string 
 
+decodingResult : Result String String
 decodingResult = 
     decodeString decoder json
-    
-view result = 
-    case result of 
-        Ok value -> 
-            text (toString value) 
-            
-        Err err -> 
-            text (toString err) 
-            
-main = view decodingResult
 
+
+-- ###################################################
+-- ####  VIEW  #######################################
+-- ###################################################
+
+view : Model -> Html Msg
+view model = 
+    div [ class "debug" ] [ text (toString model) ]
+
+
+-- ###################################################
+-- ####  MAIN  #######################################
+-- ###################################################
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = ( initialModel, getJson )
+        , view = view
+        , update = update
+        , subscriptions = ( \_ -> Sub.none )
+        }
+
+-- ###################################################
+-- ####  NOTES  ######################################
+-- ###################################################
+--
 -- elm-live Playground.elm --open --output=playground.js
+--
